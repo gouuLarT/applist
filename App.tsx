6d1de652c04db7ee720f58956c4254ffc5aead5b
Davi Duarte Goulart <136341251+gouuLarT@users.noreply.github.com>
2023-08-03 15:09:07 -0300

    first app
    first app to catch a list of participants for an event, with react native

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import Home   from './src/screens/home'
+import { StatusBar }  from 'react-native'
+
+export default function App() {
+  return (
+    <>
+      <StatusBar 
+      barStyle='light-content'
+      backgroundColor='transparent'
+      translucent
+      />
+      <Home/>
+    </>
+   );
+}
